fc200165bb4f11ab906f2b44ed4ded70d91eb4d0
Frankssss <m13917334342@163.com>
2019-09-04 22:59:56 +0800

    update

--- apps/comment/forms.py
+++ apps/comment/forms.py
@@ -9,7 +9,7 @@ from django.db.models import ObjectDoesNotExist
 class CommentForm(forms.Form):
     parent = forms.CharField(widget=forms.HiddenInput)
     post_id = forms.IntegerField(widget=forms.HiddenInput)
-    content = forms.CharField(widget=CKEditorWidget(config_name='comment_config'),attrs
+    content = forms.CharField(widget=CKEditorWidget(config_name='comment_config', attrs={'id': 'id_comment_text'}),
                               error_messages={'required': '评论内容不能为空'}
                               )
 
